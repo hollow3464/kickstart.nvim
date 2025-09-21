9b45ce058a59f90330546c3c4faf428eec94a314
Holly <daved3464@gmail.com>
2025-09-20 21:32:44 -0500

    formatters on different file

---
+++ lua/formatting.lua
@@ -0,0 +1,58 @@
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true, blade = true, twig = true, html = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
+    end,
+    formatters = {
+      mago = {
+        command = 'mago',
+        args = { 'format', '-i' },
+      },
+    },
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
+      --
+      -- You can use 'stop_after_first' to run the first available formatter from the list
+      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      twig = { 'djlint' },
+      jinja = { 'djlint' },
+      vue = { 'biome' },
+      typescript = { 'biome' },
+      javascript = { 'biome' },
+      javascriptreact = { 'biome' },
+      typescriptreact = { 'biome' },
+      json = { 'biome' },
+      jsonc = { 'biome' },
+      toml = { 'taplo' },
+      php = { 'blade-formatter', 'mago' },
+      blade = { 'blade-formatter', 'mago' },
+    },
+  },
+}
