ceb9b5673379e49b3d14ef5593c19088a0d71711
David Chamorro <daved3464@gmail.com>
2026-03-17 01:11:30 -0500

    update linters

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -5,9 +5,18 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+
+      -- lint.linters.mago_linter = require 'lint.mago-linter'
+      -- lint.linters.mago_analyzer = require 'lint.mago-analyzer'
+
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        php = { 'phpstan' },
+        php = { 'mago_lint', 'mago_analyze' },
+        typescript = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
+        javascript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        -- vue = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -45,6 +54,7 @@ return {
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
