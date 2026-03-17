a9f71f6570b8278453ba2dee4f63f71c39950e53
David Chamorro <daved3464@gmail.com>
2026-03-17 01:12:05 -0500

    update formatters

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -1,3 +1,28 @@
+local formatters = {
+  lua = { 'stylua' },
+  -- Conform can also run multiple formatters sequentially
+  -- python = { "isort", "black" },
+  --
+  -- You can use 'stop_after_first' to run the first available formatter from the list
+  -- javascript = { "prettierd", "prettier", stop_after_first = true },
+  xml = { 'xmlformatter' },
+  html = { 'djlint', 'rustywind' },
+  twig = { 'djlint', 'rustywind' },
+  jinja = { 'djlint' },
+  vue = { 'rustywind', 'prettierd' },
+  typescript = { 'prettierd' },
+  javascript = { 'prettierd' },
+  javascriptreact = { 'rustywind', 'prettierd' },
+  typescriptreact = { 'rustywind', 'prettierd' },
+  json = { 'prettierd' },
+  jsonc = { 'prettierd' },
+  toml = { 'taplo' },
+  php = { 'rustywind', 'mago' },
+  blade = { 'rustywind', 'blade-formatter', 'mago' },
+  hcl = { 'hclfmt' },
+  sql = { 'sqlfmt' },
+  yaml = { 'prettierd' },
+}
 return { -- Autoformat
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
@@ -18,7 +43,7 @@ return { -- Autoformat
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true, blade = true, twig = true, html = true }
+      local disable_filetypes = { c = true, cpp = true, blade = true, twig = true, html = true, php = true }
       if disable_filetypes[vim.bo[bufnr].filetype] then
         return nil
       else
@@ -34,25 +59,6 @@ return { -- Autoformat
         args = { 'format', '-i' },
       },
     },
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      twig = { 'djlint' },
-      jinja = { 'djlint' },
-      vue = { 'biome' },
-      typescript = { 'biome' },
-      javascript = { 'biome' },
-      javascriptreact = { 'biome' },
-      typescriptreact = { 'biome' },
-      json = { 'biome' },
-      jsonc = { 'biome' },
-      toml = { 'taplo' },
-      php = { 'rustywind', 'blade-formatter', 'mago' },
-      blade = { 'rustywind', 'blade-formatter', 'mago' },
-    },
+    formatters_by_ft = formatters,
   },
 }
